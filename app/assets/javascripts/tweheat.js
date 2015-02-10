245eed371d5158702e00cf3d64ef002d268ad221
dcurletti <curlettid@gmail.com>
2015-02-10 10:58:46 -0800

    Trying to use regular websockets

--- app/assets/javascripts/tweheat.js
+++ app/assets/javascripts/tweheat.js
@@ -11,9 +11,30 @@ window.Tweheat = {
 
 		//Top level access so that all layered subviews can access the stream
 		// this.twitterStream = new EventSource("/tweets/stream");
-		var url = "wss://" + window.location.host + '/websocket';
-		this.dispatcher = new WebSocketRails( url );
-		debugger
+		var url = "ws://" + window.location.host + '/websocket';
+		// this.dispatcher = new WebSocketRails( url );
+
+		var ws = new WebSocket( url );
+		ws.onopen = function()
+		{
+		  // Web Socket is connected, send data using send()
+		  ws.send("Message to send");
+		  // alert("Message is sent...");
+		};
+		ws.onmessage = function (evt) 
+		{ 
+		  var received_msg = evt.data;
+		  console.log(received_msg)
+		};
+		ws.onclose = function()
+		{ 
+		  // websocket is closed.
+		  alert("Connection is closed..."); 
+		};
+
+
+
+
 		//TEMP: 
 		console.log( "Establishing websocket connection on: " + url )
 
